2488dd33cb77965e031dfa83a8c914648ccad22a
Code JSON <73917308+codragon2020@users.noreply.github.com>
2022-01-24 06:44:54 -0500

    initial commit

---
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,22 @@
+package com.rest.webservices.restfulwebservices.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+	
+	@Autowired
+	private UserDaoService service;
+	
+	//GET /users
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers(){
+		return service.findAll();
+	}
+	
+	
+}
