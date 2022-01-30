6f8a123bc2aa008fb929358e70b3ac4f2de67489
Code JSON <73917308+codragon2020@users.noreply.github.com>
2022-01-30 07:08:30 -0500

    Getting posts by user and setting posts by user.

--- restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/User.java
@@ -1,11 +1,20 @@
 package com.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+@Entity
 public class User {
+	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
 	@Size(min=2, message="Name should have at least 2 characters")
@@ -14,11 +23,13 @@ public class User {
 	@Past(message="Birthdate should be in the past")
 	private Date birthDate;
 	
-	//Instructor uses a default no argument constructor
-	//I do not seem to need this...
-//	protected User() {
-//		
-//	}
+	@OneToMany(mappedBy="user")
+	private List<Post> posts;
+
+	//This became a requirement after we hooked up the JPA service to H2
+	protected User() {
+		
+	}
 	
 	public User(Integer id, String name, Date birthDate) {
 		super();
@@ -49,6 +60,14 @@ public class User {
 	
 	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
+	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 	
 	@Override
---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user values(101, sysdate(), 'AB');
+insert into user values(102, sysdate(), 'Jill');
+insert into user values(103, sysdate(), 'James');
+insert into post values(1001, 'My first post', 101);
+insert into post values(1002, 'My second post', 101);
